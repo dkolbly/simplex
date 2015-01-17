4090db0c71ab4c4cb136e8e04c63011207f3968d
Donovan Kolbly <donovan@rscheme.org>
2015-01-17 09:35:48 -0600

    Added unit tests for Noise3 and Noise4

--- simplex_test.go
+++ simplex_test.go
@@ -21,6 +21,95 @@ func TestSimplex2(t *testing.T) {
 	if math.Abs(a-a2) < 0.0001 {
 		t.Errorf("got %.4f and %.4f with different seeds", a, a2)
 	}
+
+	var minValue, maxValue float64
+
+	for i := 0; i < 1000000; i++ {
+		x := r.Float64()
+		y := r.Float64()
+
+		a := n.Noise2(x, y)
+		if i == 0 {
+			minValue = a
+			maxValue = a
+		} else {
+			if a < minValue {
+				minValue = a
+			}
+			if a > maxValue {
+				maxValue = a
+			}
+		}
+	}
+	if minValue < -1 {
+		t.Errorf("got min value %.4f, expected no less than -1", minValue)
+	}
+	if maxValue > 1 {
+		t.Errorf("got max value %.4f, expected no more than 1", maxValue)
+	}
+}
+
+func TestSimplex3(t *testing.T) {
+	r := rand.New(rand.NewSource(101))
+	n := New(r)
+	var minValue, maxValue float64
+
+	for i := 0; i < 1000000; i++ {
+		x := r.Float64()
+		y := r.Float64()
+		z := r.Float64()
+
+		a := n.Noise3(x, y, z)
+		if i == 0 {
+			minValue = a
+			maxValue = a
+		} else {
+			if a < minValue {
+				minValue = a
+			}
+			if a > maxValue {
+				maxValue = a
+			}
+		}
+	}
+	if minValue < -1 {
+		t.Errorf("got min value %.4f, expected no less than -1", minValue)
+	}
+	if maxValue > 1 {
+		t.Errorf("got max value %.4f, expected no more than 1", maxValue)
+	}
+}
+
+func TestSimplex4(t *testing.T) {
+	r := rand.New(rand.NewSource(101))
+	n := New(r)
+	var minValue, maxValue float64
+
+	for i := 0; i < 1000000; i++ {
+		x := r.Float64()
+		y := r.Float64()
+		z := r.Float64()
+		w := r.Float64()
+
+		a := n.Noise4(x, y, z, w)
+		if i == 0 {
+			minValue = a
+			maxValue = a
+		} else {
+			if a < minValue {
+				minValue = a
+			}
+			if a > maxValue {
+				maxValue = a
+			}
+		}
+	}
+	if minValue < -1 {
+		t.Errorf("got min value %.4f, expected no less than -1", minValue)
+	}
+	if maxValue > 1 {
+		t.Errorf("got max value %.4f, expected no more than 1", maxValue)
+	}
 }
 
 // on my machine (charon) we get about 145 ns/op
